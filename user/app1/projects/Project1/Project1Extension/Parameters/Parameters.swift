c306178990a2bd5905bf4c158863f59dbc403632
yahiro <yahiro07@users.noreply.github.com>
2026-02-22 00:46:57 +0900

    create parameter definitions provider

--- user/app1/projects/Project1/Project1Extension/Parameters/Parameters.swift
+++ user/app1/projects/Project1/Project1Extension/Parameters/Parameters.swift
@@ -1,75 +1,28 @@
 import AudioToolbox
 import Foundation
 
-extension ParameterGroupSpec {
-  init(identifier: String, name: String, children: [NodeSpec]) {
-    self.init(identifier: identifier, name: name) {
-      return children
-    }
+class ParameterSpecProvider {
+  let latestParametersVersion: Int = 1
+
+  func migrateParametersIfNeeded(paramVer: Int, rawParameters: inout [String: Float]) {
+    //invoked when parameters are loaded, apply your own migration if needed
   }
-}
 
-// let debugFallbackParameterSpecs = ParameterTreeSpec {
-//   ParameterGroupSpec(identifier: "global", name: "Global") {
-//     ParameterSpec(
-//       // address: .gain,
-//       address: 0,
-//       identifier: "gain",
-//       name: "Output Gain",
-//       units: .linearGain,
-//       valueRange: 0.0...1.0,
-//       defaultValue: 0.25
-//     )
-//   }
-// }
-
-func createProject1ExtensionParameterSpecs(_ synthInstanceHandle: SynthInstanceHandle)
-  -> ParameterTreeSpec
-{
-  if false {
-    return ParameterTreeSpec {
-      ParameterGroupSpec(identifier: "global", name: "Global") {
-        ParameterSpec(
-          // address: .gain,
-          address: 0,
-          identifier: "gain",
-          name: "Output Gain",
-          units: .linearGain,
-          valueRange: 0.0...1.0,
-          defaultValue: 0.25
-        )
-      }
-    }
-  } else {
-
-    var parameterBuilder = ParameterBuilderImpl()
-    parameterBuilder.callSetupParameters(synthInstanceHandle)
-
-    let parameterSpecs = ParameterTreeSpec {
-      ParameterGroupSpec(
-        identifier: "global",
-        name: "Global",
-        children: parameterBuilder.getItems().map { item in
-          ParameterSpec(
-            address: item.address,
-            identifier: String(item.identifier),
-            name: String(item.label),
-            units: item.type == .Enum ? .indexed : (item.type == .Bool ? .boolean : .generic),
-            valueRange: item.minValue...item.maxValue,
-            defaultValue: item.defaultValue,
-            unitName: nil,
-            flags: item.type == .Enum
-              ? [
-                .flag_IsWritable, .flag_IsReadable,
-                .flag_ValuesHaveStrings,
-              ] : [.flag_IsWritable, .flag_IsReadable],
-            valueStrings: item.valueStrings.empty()
-              ? nil : item.valueStrings.map { String($0) },
-            dependentParameters: nil
-          )
-        })
-    }
-    print(parameterSpecs)
-    return parameterSpecs
+  func setupParameters(_ synthInstanceHandle: SynthInstanceHandle) -> ParameterTreeSpec {
+    return pullParameterDefinitionsFromCppSide(synthInstanceHandle)
+    //if you want to make more flexible parameter tree than C++ side definition,
+    //write parameters directly here instead of calling pullParameterDefinitionsFromCppSide().
+    // return ParameterTreeSpec {
+    // ParameterGroupSpec(identifier: "global", name: "Global") {
+    //   ParameterSpec(
+    //     address: .gain,
+    //     identifier: "gain",
+    //     name: "Output Gain",
+    //     units: .linearGain,
+    //     valueRange: 0.0...1.0,
+    //     defaultValue: 0.25
+    //   )
+    // }
+    // }
   }
 }
