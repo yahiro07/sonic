174efe5eede06f207b75a816337e4f789525b46b
yahiro <yahiro07@users.noreply.github.com>
2026-02-22 02:47:30 +0900

    remove comment out code

--- user/app1/projects/Project1/Project1Extension/DSP/SynthesizerBase.hpp
+++ user/app1/projects/Project1/Project1Extension/DSP/SynthesizerBase.hpp
@@ -10,8 +10,6 @@ using StrVec = const std::vector<std::string> &;
 
 class ParameterBuilder {
 public:
-  // ParameterBuilder *asParameterBuilder() { return this; }
-
   virtual ~ParameterBuilder() = default;
   virtual void addUnary(uint64_t address, Str identifier, Str label,
                         float defaultValue) = 0;
@@ -23,13 +21,11 @@ public:
 
 class SynthesizerBase {
 public:
-  // SynthesizerBase *asSynthesizerBase() { return this; }
-
   virtual ~SynthesizerBase() = default;
   virtual void setupParameters(ParameterBuilder &builder) = 0;
   virtual void setParameter(uint64_t address, float value) = 0;
   virtual void prepare(float sampleRate, int32_t maxFrameCount) = 0;
   virtual void noteOn(int32_t noteNumber, float velocity) = 0;
   virtual void noteOff(int32_t noteNumber) = 0;
-  virtual void process(float *bufferL, float* bufferR, int32_t frames) = 0;
+  virtual void process(float *bufferL, float *bufferR, int32_t frames) = 0;
 };
