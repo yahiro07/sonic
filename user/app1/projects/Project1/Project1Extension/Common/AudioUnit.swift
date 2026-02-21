0f409b80dc7e33e804de0b5bc8156f4e3ee15da3
yahiro <yahiro07@users.noreply.github.com>
2026-02-21 23:43:29 +0900

    update audiounit

--- user/app1/projects/Project1/Project1Extension/Common/AudioUnit.swift
+++ user/app1/projects/Project1/Project1Extension/Common/AudioUnit.swift
@@ -1,4 +1,5 @@
 import AVFoundation
+import CoreAudioKit
 
 typealias SynthInstanceHandle = UnsafeMutablePointer<SynthesizerBase>
 
@@ -13,9 +14,14 @@ public class AudioUnit: AUAudioUnit, @unchecked Sendable {
 
   private var format: AVAudioFormat
 
+  let stateKvs = StateKvs()
+  private(set) var isHostedInStandaloneApp: Bool = false
+  private(set) var currentPresetParametersVersion: Int = 0
+
   @objc override init(
     componentDescription: AudioComponentDescription, options: AudioComponentInstantiationOptions
   ) throws {
+    logger.log("GenericExtensionAudioUnit init")
     self.format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 2)!
     try super.init(componentDescription: componentDescription, options: options)
     outputBus = try AUAudioUnitBus(format: self.format)
@@ -24,6 +30,28 @@ public class AudioUnit: AUAudioUnit, @unchecked Sendable {
       audioUnit: self, busType: AUAudioUnitBusType.output, busses: [outputBus!])
     processHelper = AUProcessHelper(&kernel)
     kernel.setSynthesizerInstance(synthInstance)
+  }
+
+  public override func supportedViewConfigurations(
+    _ availableViewConfigurations: [AUAudioUnitViewConfiguration]
+  ) -> IndexSet {
+    return IndexSet(integersIn: 0..<availableViewConfigurations.count)
+  }
+
+  func pullRTAudioPortalEventOne() -> DownstreamEvent? {
+    var rawEvent = RealtimeHostEvent()
+    guard processHelper?.popRealtimeHostEvent(&rawEvent) == true else {
+      return nil
+    }
+    return mapRealtimeHostEventToDownstreamEvent(rawEvent)
+  }
+
+  //push MIDI note sent from internal UI
+  func pushScheduledMidiEvent(_ bytes: [UInt8]) {
+    guard let midiBlock = scheduleMIDIEventBlock else { return }
+    midiBlock(
+      AUEventSampleTimeImmediate, 0, Int(bytes.count), bytes
+    )
   }
 
   public override var outputBusses: AUAudioUnitBusArray {
@@ -88,6 +116,11 @@ public class AudioUnit: AUAudioUnit, @unchecked Sendable {
     let parameterTree = parameterSpecs.createAUParameterTree()
     self.parameterTree = parameterTree
 
+    let maxAddress = parameterTree.allParameters.map { $0.address }.max() ?? 0
+    let capacity64 = maxAddress &+ 1
+    let capacity = UInt32(min(capacity64, UInt64(UInt32.max)))
+    kernel.setParameterCapacity(capacity)
+
     // Set the Parameter default values before setting up the parameter callbacks
     for param in parameterTree.allParameters {
       kernel.setParameter(param.address, param.value)
@@ -115,4 +148,66 @@ public class AudioUnit: AUAudioUnit, @unchecked Sendable {
       return NSString.localizedStringWithFormat("%.f", value) as String
     }
   }
+
+  func emitParametersState() -> (parametersVersion: Int, parameters: [String: Float]) {
+    // let parametersVersion = self.pluginCore?.parametersMigrator?.latestParametersVersion ?? 0
+    let parametersVersion = 0
+    var rawParameters: [String: Float] = [:]
+    parameterTree?.allParameters.forEach { param in
+      rawParameters[param.identifier] = param.value
+    }
+    return (parametersVersion, rawParameters)
+  }
+
+  func applyParametersState(
+    _ parametersVersion: Int, _ parameters: [String: Float]
+  ) {
+    // var modParameters = parameters
+    // self.pluginCore?.parametersMigrator?.migrateParametersIfNeeded(
+    //   paramVer: parametersVersion, rawParameters: &modParameters)
+    parameterTree?.allParameters.forEach { param in
+      if let value = parameters[param.identifier] {
+        param.value = value
+      }
+    }
+    self.currentPresetParametersVersion = parametersVersion
+    kernel.setParametersVersion(Int32(parametersVersion))
+  }
+
+  public override var fullState: [String: Any]? {
+    get {
+      logger.mark("fullSaving saving")
+      let baseState = super.fullState
+      var state: [String: Any] = [
+        "type": componentDescription.componentType,
+        "subtype": componentDescription.componentSubType,
+        "manufacturer": componentDescription.componentManufacturer,
+        "version": baseState?["version"] as? Int ?? 0,
+      ]
+      state["kvsItems"] = stateKvs.items
+      let (parametersVersion, parameters) = emitParametersState()
+      state["parametersVersion"] = parametersVersion
+      state["parameters"] = parameters
+      return state
+    }
+
+    set(newValue) {
+      logger.mark("fullState restoration")
+      guard let state = newValue else { return }
+      if let flag = state["MySynth1.hostedInStandaloneApp"] as? Bool {
+        self.isHostedInStandaloneApp = flag
+      }
+      if let parametersVersion = state["parametersVersion"] as? Int,
+        let parameters = state["parameters"] as? [String: Float]
+      {
+        applyParametersState(parametersVersion, parameters)
+      }
+      if let kvsItems = state["kvsItems"] as? [String: String] {
+        stateKvs.setItems(kvsItems)
+      }
+      //skipping super.fullState to avoid overwriting our custom restoration results.
+      // super.fullState = state
+    }
+  }
+
 }
