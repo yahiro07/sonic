77f244054338be1fbda28a3184210f8c4b01d3ee
yahiro <yahiro07@users.noreply.github.com>
2026-02-21 09:38:50 +0900

    add dev project

---
+++ user/app1/projects/MySynth2/MySynth2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySynth2
+//
+//  Created by ore on 2026/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ user/app1/projects/MySynth2/MySynth2/MySynth2App.swift
@@ -0,0 +1,17 @@
+//
+//  MySynth2App.swift
+//  MySynth2
+//
+//  Created by ore on 2026/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MySynth2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
