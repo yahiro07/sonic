d4abd888b7745774fdb1e4a671fd8eac03207295
yahiro <yahiro07@users.noreply.github.com>
2026-02-24 18:26:27 +0900

    add sliders

--- sonic-framework/wrappers/vst3wf/resource/www/assets/index.js
+++ sonic-framework/wrappers/vst3wf/resource/www/assets/index.js
@@ -34,3 +34,45 @@ setTimeout(() => {
   );
   pushLine("C ON")
 }, 3000)
+
+function sendMessage(msg){
+  window.webkit.messageHandlers.native.postMessage(
+    JSON.stringify({ detail: msg })
+  );
+}
+
+function sendParameter(id, value){
+  sendMessage({
+    type: "setParameter",
+    id: id,
+    value: value
+  }) 
+}
+
+
+function addSlider(name, id, defaultValue){
+  const slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = 0;
+  slider.max = 1;
+  slider.value = defaultValue;
+  slider.id = id;
+  slider.oninput = () => {
+    sendParameter(id, slider.value);
+  };
+  // document.body.appendChild(slider);
+
+  const label = document.createElement("label");
+  label.innerText = name;
+
+  const div = document.createElement("div");
+  div.style.display = "flex";
+  div.appendChild(label);
+  div.appendChild(slider);
+  document.body.appendChild(div);
+}
+
+addSlider("Gain", 0, 0.5);
+addSlider("Pitch", 1, 0.5);
+addSlider("Volume", 2, 0.5);
+
