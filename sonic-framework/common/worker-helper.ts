4a6a50df1783356e77f34c41fba0430af57be08f
yahiro <yahiro07@users.noreply.github.com>
2026-02-26 05:23:54 +0900

    create project copy task base

--- sonic-framework/common/worker-helper.ts
+++ sonic-framework/common/worker-helper.ts
@@ -23,3 +23,37 @@ export function workerHelper_copyProjectContent(
 		recursive: true,
 	});
 }
+
+export function workerHelper_updateFileNamesRecursive(
+	folderPath: string,
+	spec: {
+		// extensions?: string[];
+		filesMatcher?: string;
+		replacements: {
+			// forwardChars?: string[];
+			originalPrefix: string;
+			newPrefix: string;
+		}[];
+	},
+) {}
+
+export function workerHelper_replaceInFileSignaturesRecursive(
+	folderPath: string,
+	spec: {
+		// extensions?: string[];
+		filesMatcher?: string;
+		replacements: {
+			// forwardChars?: string[];
+			originalPrefix: string;
+			newPrefix: string;
+		}[];
+	},
+) {}
+
+export function casingToSnake(str: string) {
+	return str.replace(/([A-Z])/g, "_$1").toLowerCase();
+}
+
+export function casingToCapital(str: string) {
+	return str.replace(/([a-z])/g, "_$1").toLowerCase();
+}
--- sonic-framework/templates/vst-simple/worker/index.ts
+++ sonic-framework/templates/vst-simple/worker/index.ts
@@ -1,9 +1,43 @@
-import { workerHelper_copyProjectContent } from "../../../common/worker-helper";
+import path from "path";
+import {
+	casingToCapital,
+	casingToSnake,
+	workerHelper_copyProjectContent,
+	workerHelper_replaceInFileSignaturesRecursive,
+	workerHelper_updateFileNamesRecursive,
+} from "../../../common/worker-helper";
 import type { WorkerInterface } from "../../../common/worker-types";
 
 export const template_vstSimple_worker: WorkerInterface = {
 	async createProject(projectName, templateName) {
 		workerHelper_copyProjectContent(projectName, templateName);
+
+		const newFolderPath = path.join(process.cwd(), projectName);
+
+		const projectNameInFilePath = casingToSnake(projectName);
+
+		const projectNameInSignatures = casingToCapital(projectName);
+
+		workerHelper_updateFileNamesRecursive(newFolderPath, {
+			filesMatcher: "source/vst/**/*",
+			replacements: [
+				{ originalPrefix: "project1", newPrefix: projectNameInFilePath },
+			],
+		});
+
+		workerHelper_replaceInFileSignaturesRecursive(newFolderPath, {
+			filesMatcher: "source/vst/**/*",
+			replacements: [
+				{ originalPrefix: "project1", newPrefix: projectNameInFilePath },
+			],
+		});
+
+		workerHelper_replaceInFileSignaturesRecursive(newFolderPath, {
+			filesMatcher: "source/vst/**/*",
+			replacements: [
+				{ originalPrefix: "Project1", newPrefix: projectNameInSignatures },
+			],
+		});
 		return true;
 	},
 };
