b2c86c107176cdb21daf15859596f46905bbe717
yahiro <yahiro07@users.noreply.github.com>
2026-02-28 05:22:30 +0900

    change icon

--- templates/vst-simple/frontend/src/components/ToneButton.tsx
+++ templates/vst-simple/frontend/src/components/ToneButton.tsx
@@ -26,7 +26,20 @@ export const ToneButton: FC<Props> = ({ label, onPress, onRelease }) => {
         css={[styles.pad, isPressed && styles.padPressed]}
         onPointerDown={handlePointerDown}
       >
-        <img src="/vite.svg" />
+        {/* grommet-icons:trigger */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={32}
+          height={32}
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            d="M4 14h6l-3 9h2L20 9h-6l4-8H7z"
+          ></path>
+        </svg>
       </div>
       <div css={styles.label}>{label}</div>
     </div>
