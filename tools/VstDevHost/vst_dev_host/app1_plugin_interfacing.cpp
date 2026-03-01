7b202e92548304d2384d8b1a16df19b768a59c3f
yahiro <yahiro07@users.noreply.github.com>
2026-03-02 06:41:19 +0900

    instantiate processor

--- tools/VstDevHost/vst_dev_host/app1_plugin_interfacing.cpp
+++ tools/VstDevHost/vst_dev_host/app1_plugin_interfacing.cpp
@@ -1,13 +1,13 @@
-// #include "public.sdk/source/vst/hosting/module.h"
-// #include "public.sdk/source/vst/hosting/plugprovider.h"
-#include "public.sdk/source/vst/hosting/hostclasses.h"
-// #include "public.sdk/source/vst/hosting/eventlist.h"
-// #include "public.sdk/source/vst/hosting/parameterchanges.h"#include <stdio.h>
 #include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/base/ipluginbase.h"
+#include "pluginterfaces/vst/ivstaudioprocessor.h"
+#include "public.sdk/source/vst/hosting/hostclasses.h"
 #include "public.sdk/source/vst/hosting/module.h"
+#include "public.sdk/source/vst/hosting/plugprovider.h"
 #include <filesystem>
 
 using namespace Steinberg;
+using namespace Steinberg::Vst;
 
 void app1Entry() {
   printf("app1Entry\n");
@@ -25,5 +25,32 @@ void app1Entry() {
     printf("Failed to create module: %s\n", errorDescription.c_str());
     return;
   }
+  auto factory = module->getFactory();
+  VST3::Hosting::ClassInfo audioEffectClassInfo;
+  for (auto &classInfo : factory.classInfos()) {
+    printf("Class: %s %s\n", classInfo.name().c_str(),
+           classInfo.category().c_str());
+    if (classInfo.category() == kVstAudioEffectClass) {
+      audioEffectClassInfo = classInfo;
+      break;
+    }
+  }
+  auto plugProvider = new PlugProvider(factory, audioEffectClassInfo, true);
+  if (!plugProvider) {
+    printf("Failed to create plug provider\n");
+    return;
+  }
+  auto component = plugProvider->getComponent();
+  if (!component) {
+    printf("Failed to create vst plug\n");
+    return;
+  }
+
+  auto audioProcessor = FUnknownPtr<IAudioProcessor>(component);
+  if (!audioProcessor) {
+    printf("Failed to query audio processor\n");
+    return;
+  }
+
   printf("loaded\n");
 }
