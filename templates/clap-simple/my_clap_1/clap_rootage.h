f2263ffa53d533680fac64ef446a0058ce3826b0
yahiro <yahiro07@users.noreply.github.com>
2026-03-05 11:09:07 +0900

    create abi translation layer

---
+++ templates/clap-simple/my_clap_1/clap_rootage.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <clap/clap.h>
+#include <functional>
+
+class PlugBasis {
+public:
+  clap_plugin_t clapPlugin;
+
+  virtual ~PlugBasis() = default;
+  virtual void setSampleRate(double sampleRate) = 0;
+  virtual clap_process_status process(const clap_process_t *processData) = 0;
+
+  virtual uint32_t getParameterCount() const = 0;
+  virtual void getParameterInfo(uint32_t index,
+                                clap_param_info_t *info) const = 0;
+  virtual double getParameterValue(clap_id id) const = 0;
+  virtual void setParameterValue(clap_id id, double value) = 0;
+
+  virtual bool guiCreate() = 0;
+  virtual void guiDestroy() = 0;
+  virtual bool guiSetParent(const clap_window_t *window) = 0;
+  virtual bool guiSetSize(uint32_t width, uint32_t height) = 0;
+  virtual bool guiShow() = 0;
+  virtual bool guiHide() = 0;
+};
+
+void clapRootage_setPlugBasisInstantiateFn(std::function<PlugBasis *()> fn);
+clap_plugin_descriptor_t &clapRootage_getPluginDescriptor();
+const clap_plugin_entry_t &clapRootage_getClapPluginEntry();
--- templates/clap-simple/my_clap_1/clap_wrapper.h
+++ templates/clap-simple/my_clap_1/clap_wrapper.h
@@ -1,26 +1,6 @@
 #pragma once
-
+#include "synthesizer_base.h"
 #include <clap/clap.h>
-#include <cstdint>
-
-class SynthesizerBase {
-public:
-  virtual ~SynthesizerBase() = default;
-  virtual void setSampleRate(double sampleRate) = 0;
-  virtual void processAudio(float *bufferL, float *bufferR,
-                            uint32_t frames) = 0;
-  virtual void noteOn(int noteNumber, double velocity) = 0;
-  virtual void noteOff(int noteNumber) = 0;
-
-  // these are temporal interface
-  virtual uint32_t getParameterCount() const = 0;
-  virtual void getParameterInfo(uint32_t index,
-                                clap_param_info_t *info) const = 0;
-  virtual double getParameterValue(clap_id id) const = 0;
-  virtual void setParameterValue(clap_id id, double value) = 0;
-};
-
-typedef SynthesizerBase *(*SynthesizerInitializerFn)();
 
 struct PluginMeta {
   const char *id;
