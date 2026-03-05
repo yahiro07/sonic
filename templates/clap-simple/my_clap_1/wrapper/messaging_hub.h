1e31115d1e0635b008ab49fa43ba840a0d3f7282
yahiro <yahiro07@users.noreply.github.com>
2026-03-06 04:19:22 +0900

    decode parameter message

--- templates/clap-simple/my_clap_1/wrapper/messaging_hub.h
+++ templates/clap-simple/my_clap_1/wrapper/messaging_hub.h
@@ -32,16 +32,66 @@ public:
   void stop() { webViewIo.setMessageReceiver(nullptr); }
 };
 
+struct RxMsgLog {
+  std::string type = "log";
+  double timestamp;
+  std::string logKind;
+  std::string message;
+};
+struct RxMsgUiLoaded {
+  std::string type = "uiLoaded";
+};
+struct RxMsgBeginEdit {
+  std::string type = "beginEdit";
+  std::string identifier;
+};
+struct RxMsgPerformEdit {
+  std::string type = "applyEdit";
+  std::string identifier;
+  double value;
+};
+struct RxMsgEndEdit {
+  std::string type = "endEdit";
+  std::string identifier;
+};
+struct RxMsgInstantEdit {
+  std::string type = "instantEdit";
+  std::string identifier;
+  double value;
+};
+struct RxMsgNoteOnRequest {
+  std::string type = "noteOnRequest";
+  int noteNumber;
+};
+struct RxMsgNoteOffRequest {
+  std::string type = "noteOffRequest";
+  int noteNumber;
+};
+
+using RxMessageVariant =
+    std::variant<RxMsgLog, RxMsgUiLoaded, RxMsgBeginEdit, RxMsgPerformEdit,
+                 RxMsgEndEdit, RxMsgInstantEdit, RxMsgNoteOnRequest,
+                 RxMsgNoteOffRequest>;
+namespace glz {
+template <> struct meta<RxMessageVariant> {
+  static constexpr std::string_view tag = "type";
+  static constexpr auto ids = std::array{
+      "log",     "uiLoaded",    "beginEdit",     "applyEdit",
+      "endEdit", "instantEdit", "noteOnRequest", "noteOffRequest",
+  };
+};
+} // namespace glz
+
 inline void messagingHub_dev_handleMessageFromUi(
-    const std::string &message,
-    std::function<void(uint32_t, double)> setParameterFromUi) {
-  printf("message: %s\n", message.c_str());
+    const std::string &jsonStr,
+    std::function<void(std::string &, double)> setParameterFromUi) {
+  printf("message: %s\n", jsonStr.c_str());
 
-  glz::generic json;
-
-  if (true) {
-    uint32_t paramId = 0;
-    double paramValue = rand() / (double)RAND_MAX; // debug;
-    setParameterFromUi(paramId, paramValue);
+  RxMessageVariant rxMessage;
+  auto ec = glz::read_json<RxMessageVariant>(rxMessage, jsonStr);
+  if (ec)
+    return;
+  if (auto *m = std::get_if<RxMsgPerformEdit>(&rxMessage)) {
+    setParameterFromUi(m->identifier, m->value);
   }
 }
