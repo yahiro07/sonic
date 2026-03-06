3d4698ff53ccf4072a440742de84bfdc26fecf93
yahiro <yahiro07@users.noreply.github.com>
2026-03-06 11:09:50 +0900

    separate classes

--- templates/clap-simple/my_clap_1/rootage/clap_rootage.h
+++ templates/clap-simple/my_clap_1/rootage/clap_rootage.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "clap/clap.h"
+#include "clap/host.h"
 #include "clap/plugin.h"
 #include <functional>
 
@@ -11,15 +12,18 @@ public:
   const clap_host_params_t *hostParams = nullptr;
 
   virtual ~PlugBasis() = default;
+
+  virtual void initialize() = 0; // called after host and hostParams are set
+
   virtual void setSampleRate(double sampleRate) = 0;
   virtual clap_process_status process(const clap_process_t *processData) = 0;
+  virtual void flushParameters(const clap_input_events_t *inputEvents,
+                               const clap_output_events_t *outputEvents) = 0;
 
   virtual uint32_t getParameterCount() const = 0;
   virtual void getParameterInfo(uint32_t index,
                                 clap_param_info_t *info) const = 0;
   virtual double getParameterValue(clap_id id) const = 0;
-  virtual void flushParameters(const clap_input_events_t *inputEvents,
-                               const clap_output_events_t *outputEvents) = 0;
 
   virtual bool guiCreate() = 0;
   virtual void guiDestroy() = 0;
