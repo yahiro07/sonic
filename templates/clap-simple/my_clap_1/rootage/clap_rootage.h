817657047eafa0e20275021dc61132b15c49f73a
yahiro <yahiro07@users.noreply.github.com>
2026-03-06 11:26:15 +0900

    separate files

--- templates/clap-simple/my_clap_1/rootage/clap_rootage.h
+++ templates/clap-simple/my_clap_1/rootage/clap_rootage.h
@@ -1,39 +1,8 @@
 #pragma once
 
+#include "./plug_basis.h"
 #include "clap/clap.h"
-#include "clap/host.h"
-#include "clap/plugin.h"
 #include <functional>
-
-class PlugBasis {
-public:
-  clap_plugin_t plugin;
-  const clap_host_t *host = nullptr;
-  const clap_host_params_t *hostParams = nullptr;
-
-  virtual ~PlugBasis() = default;
-
-  virtual void initialize() = 0; // called after host and hostParams are set
-
-  virtual void setSampleRate(double sampleRate) = 0;
-  virtual clap_process_status process(const clap_process_t *processData) = 0;
-  virtual void flushParameters(const clap_input_events_t *inputEvents,
-                               const clap_output_events_t *outputEvents) = 0;
-
-  virtual uint32_t getParameterCount() const = 0;
-  virtual void getParameterInfo(uint32_t index,
-                                clap_param_info_t *info) const = 0;
-  virtual double getParameterValue(clap_id id) const = 0;
-
-  virtual bool guiCreate() = 0;
-  virtual void guiDestroy() = 0;
-  virtual bool guiSetParent(const clap_window_t *window) = 0;
-  virtual bool guiSetSize(uint32_t width, uint32_t height) = 0;
-  virtual bool guiShow() = 0;
-  virtual bool guiHide() = 0;
-
-  virtual void onMainThread() {}
-};
 
 void clapRootage_setPluginBasisInstantiateFn(
     const std::function<PlugBasis *()> fn);
---
+++ templates/clap-simple/my_clap_1/rootage/plug_basis.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "clap/clap.h"
+
+class PlugBasis {
+public:
+  clap_plugin_t plugin;
+  const clap_host_t *host = nullptr;
+  const clap_host_params_t *hostParams = nullptr;
+
+  virtual ~PlugBasis() = default;
+
+  virtual void initialize() = 0; // called after host and hostParams are set
+
+  virtual void setSampleRate(double sampleRate) = 0;
+  virtual clap_process_status process(const clap_process_t *processData) = 0;
+  virtual void flushParameters(const clap_input_events_t *inputEvents,
+                               const clap_output_events_t *outputEvents) = 0;
+
+  virtual uint32_t getParameterCount() const = 0;
+  virtual void getParameterInfo(uint32_t index,
+                                clap_param_info_t *info) const = 0;
+  virtual double getParameterValue(clap_id id) const = 0;
+
+  virtual bool guiCreate() = 0;
+  virtual void guiDestroy() = 0;
+  virtual bool guiSetParent(const clap_window_t *window) = 0;
+  virtual bool guiSetSize(uint32_t width, uint32_t height) = 0;
+  virtual bool guiShow() = 0;
+  virtual bool guiHide() = 0;
+
+  virtual void onMainThread() {}
+};
