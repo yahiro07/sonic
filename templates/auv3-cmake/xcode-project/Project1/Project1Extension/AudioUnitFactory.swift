d658720312d9d2044c14038dea917fe44fe2254e
yahiro <yahiro07@users.noreply.github.com>
2026-03-09 04:39:56 +0900

    remove unused files

--- templates/auv3-cmake/xcode-project/Project1/Project1Extension/AudioUnitFactory.swift
+++ templates/auv3-cmake/xcode-project/Project1/Project1Extension/AudioUnitFactory.swift
@@ -1,27 +1,20 @@
-//
-//  AudioUnitFactory.swift
-//  Project1Extension
-//
-//  Created by ore on 2026/03/08.
-//
-
 import CoreAudioKit
 import os
 
-private let log = Logger(subsystem: "com.bundle.id.Project1Extension", category: "AudioUnitFactory")
-
 public class AudioUnitFactory: NSObject, AUAudioUnitFactory {
-    var auAudioUnit: AUAudioUnit?
+  var auAudioUnit: AUAudioUnit?
 
-    public func beginRequest(with context: NSExtensionContext) {}
+  public func beginRequest(with context: NSExtensionContext) {}
 
-    @objc
-    public func createAudioUnit(with componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
-        auAudioUnit = try WrapperAuv3AudioUnit(componentDescription: componentDescription, options: [])
-        guard let audioUnit = auAudioUnit else {
-            fatalError("Failed to create Project1Extension")
-        }
-        return audioUnit
+  @objc
+  public func createAudioUnit(with componentDescription: AudioComponentDescription) throws
+    -> AUAudioUnit
+  {
+    auAudioUnit = try WrapperAuv3AudioUnit(componentDescription: componentDescription, options: [])
+    guard let audioUnit = auAudioUnit else {
+      fatalError("Failed to create Project1Extension")
     }
-    
+    return audioUnit
+  }
+
 }
--- templates/auv3-cmake/xcode-project/Project1/Project1Extension/Project1Extension-Bridging-Header.h
+++ templates/auv3-cmake/xcode-project/Project1/Project1Extension/Project1Extension-Bridging-Header.h
@@ -1,13 +1,1 @@
-//
-//  Project1Extension-Bridging-Header.h
-//  Project1Extension
-//
-//  Created by ore on 2026/03/08.
-//
-
-#import "Project1ExtensionParameterAddresses.h"
-#import "Project1ExtensionAUProcessHelper.hpp"
-
 #import "../../../lib/wrapper-auv3/wrapper-auv3-root.h"
-
-
