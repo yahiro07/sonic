f21d1d53d3eaf0f627268139dedb079e592a033b
yahiro <yahiro07@users.noreply.github.com>
2026-03-08 19:27:54 +0900

    create base structure

---
+++ templates/auv3-cmake/lib/common/plugin_domain.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "parameter_builder_impl.h"
+#include "parameter_item.h"
+#include "synthesizer_base.h"
+#include <cstdint>
+#include <functional>
+
+class IPlatformParameterIo {
+public:
+  virtual ~IPlatformParameterIo() = default;
+  virtual void
+  registerParameters(std::vector<sonic_common::ParameterItem> &params) = 0;
+  virtual double getParameter(uint64_t address) = 0;
+  virtual void setParameter(uint64_t address, double value) = 0;
+
+  virtual void
+  setParameterChangeCallback(std::function<void(uint64_t, double)> fn) = 0;
+};
+
+class PluginDomain {
+private:
+  SynthesizerBase &synth;
+  IPlatformParameterIo &platformParameterIo;
+
+public:
+  PluginDomain(SynthesizerBase &synth,
+               IPlatformParameterIo &platformParameterIo)
+      : synth(synth), platformParameterIo(platformParameterIo) {}
+
+  void initialize() {
+    sonic_common::ParameterBuilderImpl builder;
+    synth.setupParameters(builder);
+    auto parameterItems = builder.getItems();
+    platformParameterIo.registerParameters(parameterItems);
+    for (auto &item : parameterItems) {
+      synth.setParameter(item.address, item.defaultValue);
+    }
+    platformParameterIo.setParameterChangeCallback(
+        [this](uint64_t address, double value) {
+          synth.setParameter(address, value);
+        });
+  }
+
+  void prepareProcessing(double sampleRate, uint32_t maxFrameCount) {
+    synth.prepareProcessing(sampleRate, maxFrameCount);
+  }
+
+  void processAudio(float *bufferL, float *bufferR, uint32_t frames) {
+    synth.processAudio(bufferL, bufferR, frames);
+  }
+
+  void setParameter(uint64_t address, double value) {
+    synth.setParameter(address, value);
+  }
+
+  void noteOn(int noteNumber, double velocity) {
+    synth.noteOn(noteNumber, velocity);
+  }
+
+  void noteOff(int noteNumber) { synth.noteOff(noteNumber); }
+};
---
+++ templates/auv3-cmake/plugin/project1-synthesizer.cpp
@@ -0,0 +1,61 @@
+#include "project1-synthesizer.h"
+
+void Project1Synthesizer::setupParameters(ParameterBuilder &builder) {
+  builder.addUnary(0, "gain", "Gain", 0.5);
+  builder.addEnum(1, "waveType", "Wave Type", "saw",
+                  {"saw", "square", "triangle", "sine"});
+  builder.addUnary(2, "oscPitch", "Pitch", 0.5);
+  builder.addUnary(3, "oscVolume", "Volume", 0.5);
+}
+
+void Project1Synthesizer::setParameter(uint64_t address, double value) {
+  if (address == 0) {
+    paramGain = value;
+  } else if (address == 1) {
+    paramWaveType = value;
+  } else if (address == 2) {
+    paramOscPitch = value;
+  } else if (address == 3) {
+    paramOscVolume = value;
+  }
+}
+
+void Project1Synthesizer::prepareProcessing(double sampleRate,
+                                            uint32_t maxFrameCount) {
+  this->sampleRate = (float)sampleRate;
+}
+
+void Project1Synthesizer::processAudio(float *bufferL, float *bufferR,
+                                       uint32_t frames) {
+  if (sampleRate == 0.f)
+    return;
+  auto freq = exp2f((noteNumber - 57.f) / 12.f) * 440.f;
+  auto gain = gateOn ? paramGain : 0.f;
+  auto phaseInc = freq / sampleRate;
+
+  for (uint32_t index = 0; index < frames; index++) {
+    phase += phaseInc;
+    phase -= floorf(phase);
+    // auto y = rand() / (float)RAND_MAX * 2.f - 1.f;
+    auto y = (phase * 2.f - 1.f);
+    // auto y = sinf(phase * 2.f * M_PI);
+    y *= gain;
+    bufferL[index] = y;
+    bufferR[index] = y;
+  }
+}
+
+void Project1Synthesizer::noteOn(int noteNumber, double velocity) {
+  this->noteNumber = noteNumber;
+  this->gateOn = true;
+}
+
+void Project1Synthesizer::noteOff(int noteNumber) {
+  if (noteNumber == this->noteNumber) {
+    this->gateOn = false;
+  }
+}
+
+SynthesizerBase *createSynthesizerInstance() {
+  return new Project1Synthesizer();
+}
