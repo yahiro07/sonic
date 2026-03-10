830835f35160240eddb57ba826ea32377845bf78
yahiro <yahiro07@users.noreply.github.com>
2026-03-10 14:40:47 +0900

    create parameters store

--- templates/auv3-cmake/lib/domain/interfaces.h
+++ templates/auv3-cmake/lib/domain/interfaces.h
@@ -4,6 +4,7 @@
 #include <functional>
 
 namespace sonic {
+
 using IPluginSynthesizer = SynthesizerBase;
 
 class IWebViewIo {
@@ -12,6 +13,13 @@ public:
   virtual void sendMessage(const std::string &message) = 0;
   virtual void
   setMessageReceiver(std::function<void(const std::string &)> receiver) = 0;
+};
+
+class IParametersStore {
+public:
+  virtual ~IParametersStore() = default;
+  virtual double get(uint32_t id) = 0;
+  virtual void set(uint32_t id, double value) = 0;
 };
 
 } // namespace sonic
