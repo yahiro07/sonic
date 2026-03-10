7bfaed5d4285036c5663ba67412fb0f5c7578d32
yahiro <yahiro07@users.noreply.github.com>
2026-03-10 15:10:21 +0900

    add controller facade interface

--- templates/auv3-cmake/lib/domain/interfaces.h
+++ templates/auv3-cmake/lib/domain/interfaces.h
@@ -34,7 +34,29 @@ public:
   virtual void getAllParameters(std::map<std::string, double> &parameters) = 0;
 
   virtual int subscribeParameterChange(
-      std::function<void(const std::string identifier, double value)>
+      std::function<void(const std::string paramKey, double value)>
+          callback) = 0;
+  virtual void unsubscribeParameterChange(int subscriptionId) = 0;
+};
+
+enum class ParameterEditState {
+  Begin,
+  Perform,
+  End,
+  InstantChange,
+};
+
+class IControllerFacade {
+public:
+  virtual ~IControllerFacade() = default;
+  virtual void getAllParameters(std::map<std::string, double> &parameters) = 0;
+  virtual void applyParameterEditFromUi(std::string paramKey, double value,
+                                        ParameterEditState editState) = 0;
+  virtual void requestNoteOn(int noteNumber, double velocity) = 0;
+  virtual void requestNoteOff(int noteNumber) = 0;
+
+  virtual int subscribeParameterChange(
+      std::function<void(const std::string paramKey, double value)>
           callback) = 0;
   virtual void unsubscribeParameterChange(int subscriptionId) = 0;
 };
