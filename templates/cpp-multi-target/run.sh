8053d752e797924f36e2676c5291636b424abcb8
yahiro <yahiro07@users.noreply.github.com>
2026-03-17 01:07:42 +0900

    update run.sh

--- templates/cpp-multi-target/run.sh
+++ templates/cpp-multi-target/run.sh
@@ -1,1 +1,2 @@
-../../templates/ClapDevHost/build/clap_dev_host/clap_dev_host ./build/variants/clap/project1-clap.clap
+# ../_clap-dev-host/build/clap_dev_host/clap_dev_host ./build/lib/Debug/project1-clap.clap
+../_vst-dev-host/build/bin/Debug/VstDevHost ./build/lib/Debug/project1-vst3.vst3
