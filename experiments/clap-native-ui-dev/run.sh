5a5f45d7c5405e38c5fa64a6a5bb3ee0b2a3812f
yahiro <yahiro07@users.noreply.github.com>
2026-03-23 08:33:03 +0900

    update dev host path

--- experiments/clap-native-ui-dev/run.sh
+++ experiments/clap-native-ui-dev/run.sh
@@ -1,1 +1,1 @@
-../../templates/ClapDevHost/build/clap_dev_host/clap_dev_host ./build/variants/clap/project1-clap.clap
+../../templates/_clap-dev-host/build/clap_dev_host/clap_dev_host ./build/variants/clap/project1-clap.clap
