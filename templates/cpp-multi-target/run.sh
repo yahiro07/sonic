885f470f9b2f89e0c180f6b5058c7191c0fdbc68
yahiro <yahiro07@users.noreply.github.com>
2026-03-24 22:45:07 +0900

    update presets

--- templates/cpp-multi-target/run.sh
+++ templates/cpp-multi-target/run.sh
@@ -1,2 +1,3 @@
-../_clap-dev-host/build/clap_dev_host/clap_dev_host ./build/lib/Debug/project1-clap.clap
-# ../_vst-dev-host/build/bin/Debug/VstDevHost ./build/lib/Debug/project1-vst3.vst3
+./build/ninja/bin/Debug/VstDevHost ./build/ninja/lib/Debug/project1-vst3.vst3
+# ./build/ninja/bin/Debug/clap_dev_host ./build/ninja/lib/Debug/project1-clap.clap
+
