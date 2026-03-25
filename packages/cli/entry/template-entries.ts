66db32a5af4a11225d5db05ecff2d4556bb6bf92
yahiro <yahiro07@users.noreply.github.com>
2026-03-25 12:15:41 +0900

    fix import path

--- packages/cli/entry/template-entries.ts
+++ packages/cli/entry/template-entries.ts
@@ -1,7 +1,7 @@
 import type { TemplateEntry } from "@/common/worker-types";
 import auv3SwiftXcode from "@/templates/auv3-swift-xcode/__worker";
 import vstSimple from "@/templates/vst-simple/__worker";
-import cppMultiTarget from "@/templates/cpp-multi-target/__worker";
+import cppMultiTarget from "@/workers/cpp-multi-target";
 
 export const templateEntries: TemplateEntry[] = [
   vstSimple,
