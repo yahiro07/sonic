a19f1393274c96288fc1de6ba329eb7219b876ec
yahiro <yahiro07@users.noreply.github.com>
2026-03-26 16:29:20 +0900

    drop vst-simple

--- cli/src/workers/template-entries.ts
+++ cli/src/workers/template-entries.ts
@@ -1,10 +1,8 @@
 import type { TemplateEntry } from "@/src/common/worker-types";
 import auv3SwiftXcode from "@/src/workers/auv3-swift-xcode";
-import vstSimple from "@/src/workers/vst-simple";
 import cppMultiTarget from "@/src/workers/cpp-multi-target";
 
 export const templateEntries: TemplateEntry[] = [
-  vstSimple,
   cppMultiTarget,
   auv3SwiftXcode,
 ];
