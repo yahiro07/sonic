bb3db5a7bc52cd54f1fc2820c9e218099e275281
yahiro <yahiro07@users.noreply.github.com>
2026-03-28 01:22:23 +0900

    fix order

--- templates/auv3-swift-xcode/Project1Extension/DSP/MySynthesizer.hpp
+++ templates/auv3-swift-xcode/Project1Extension/DSP/MySynthesizer.hpp
@@ -15,8 +15,9 @@ public:
   ~MySynthesizer() override = default;
 
   void setupParameters(ParameterBuilder &builder) override;
-  void setParameter(uint32_t id, double value) override;
   void prepareProcessing(double sampleRate, uint32_t maxFrameCount) override;
+
+  void setParameter(uint32_t id, double value) override;
   void noteOn(int noteNumber, double velocity) override;
   void noteOff(int noteNumber) override;
   void processAudio(float *bufferL, float *bufferR, uint32_t frames) override;
