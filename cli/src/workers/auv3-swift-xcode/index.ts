65d98e6f6315f73e5f5f75a65138bea01c48596b
yahiro <yahiro07@users.noreply.github.com>
2026-03-27 13:31:19 +0900

    update auv3 worker

--- cli/src/workers/auv3-swift-xcode/index.ts
+++ cli/src/workers/auv3-swift-xcode/index.ts
@@ -6,82 +6,151 @@ import {
   workerHelper_replaceStrings,
   workerHelper_updateFileNamesWithPrefix,
   workerHelper_getNewProjectFolderPath,
+  workerHelper_copyProjectContentFiles_withRenaming,
 } from "@/src/common";
+import * as clackPrompts from "@clack/prompts";
+
+type TemplateOptions = {
+  auManufacturer: string;
+  auSubtype: string;
+};
+
+async function readTemplateOptions(): Promise<TemplateOptions | "cancelled"> {
+  let auManufacturer: string | symbol = "";
+  let auSubtype: string | symbol = "";
+
+  const auManufacturerDefault = "Myco";
+  auManufacturer = await clackPrompts.text({
+    message: `AUv3 Manufacturer Code`,
+    placeholder: auManufacturerDefault,
+  });
+  if (clackPrompts.isCancel(auManufacturer)) {
+    return "cancelled";
+  }
+  if (auManufacturer == "") {
+    auManufacturer = auManufacturerDefault;
+  }
+  const auSubtypeDefault = generateRandomString("alphaNumeric", 4);
+  auSubtype = await clackPrompts.text({
+    message: `AUv3 Subtype`,
+    placeholder: auSubtypeDefault,
+  });
+  if (clackPrompts.isCancel(auSubtype)) {
+    return "cancelled";
+  }
+  if (auSubtype == "") {
+    auSubtype = auSubtypeDefault;
+  }
+
+  return {
+    auManufacturer,
+    auSubtype,
+  };
+}
+
+type TaskContext = {
+  projectName: string;
+  templateName: string;
+  options: TemplateOptions;
+  projectFolderPath: string;
+};
+
+function instantiateProject({
+  projectName,
+  templateName,
+  options,
+  projectFolderPath,
+}: TaskContext) {
+  workerHelper_copyProjectContentFiles(projectName, templateName, [
+    "Project1",
+    "Project1.xcodeproj/project.pbxproj",
+    "Project1Extension/Common",
+    "Project1Extension/DSP",
+    "Project1Extension/Root",
+    "Project1Extension/Info.plist",
+    "README.md",
+  ]);
+  workerHelper_copyProjectContentFiles_withRenaming(projectName, templateName, [
+    { from: ".gitignore_template", to: ".gitignore" },
+  ]);
+
+  const projectNameCapital = casingToCapital(projectName);
+
+  const extensionNameCapital = `${projectNameCapital}Extension`;
+
+  workerHelper_replaceStrings(projectFolderPath, {
+    filePaths: ["Project1/Project1App.swift"],
+    replacements: [{ from: "Project1App", to: `${projectNameCapital}App` }],
+  });
+
+  const { auSubtype, auManufacturer } = options;
+
+  workerHelper_replaceStrings(projectFolderPath, {
+    filePaths: ["Project1/Model/AudioUnitHostModel.swift"],
+    replacements: [
+      { from: `"prj1"`, to: `"${auSubtype}"` },
+      { from: `"Myco"`, to: `"${auManufacturer}"` },
+    ],
+  });
+
+  workerHelper_replaceStrings(projectFolderPath, {
+    filePaths: ["Project1Extension/info.plist"],
+    replacements: [
+      {
+        from: "<string>prj1</string>",
+        to: `<string>${auSubtype}</string>`,
+      },
+      {
+        from: "<string>Myco</string>",
+        to: `<string>${auManufacturer}</string>`,
+      },
+      {
+        from: "<string>Project1Extension</string>",
+        to: `<string>${extensionNameCapital}</string>`,
+      },
+    ],
+  });
+
+  workerHelper_replaceStrings(projectFolderPath, {
+    filePaths: ["Project1.xcodeproj/project.pbxproj"],
+    replacements: [
+      {
+        from: "Project1",
+        to: projectNameCapital,
+      },
+    ],
+  });
+
+  workerHelper_updateFileNamesWithPrefix(projectFolderPath, {
+    filePaths: [
+      "Project1/Project1App.swift",
+      "Project1/Project1.entitlements",
+      "Project1",
+      "Project1.xcodeproj",
+      "Project1Extension/Common/Project1Extension-Bridging-Header.h",
+      "Project1Extension",
+    ],
+    originalPrefix: "Project1",
+    newPrefix: projectNameCapital,
+  });
+
+  return true;
+}
 
 function createTemplateWorker(): TemplateWorker {
   return {
     async createProject(projectName, templateName) {
-      workerHelper_copyProjectContentFiles(projectName, templateName, [
-        "Project1",
-        "Project1.xcodeproj/project.pbxproj",
-        "Project1Extension/Common",
-        "Project1Extension/DSP",
-        "Project1Extension/Root",
-        "Project1Extension/Info.plist",
-        "README.md",
-      ]);
-
-      const newFolderPath = workerHelper_getNewProjectFolderPath(projectName);
-
-      const projectNameCapital = casingToCapital(projectName);
-
-      const extensionNameCapital = `${projectNameCapital}Extension`;
-
-      workerHelper_replaceStrings(newFolderPath, {
-        filePaths: ["Project1/Project1App.swift"],
-        replacements: [{ from: "Project1App", to: `${projectNameCapital}App` }],
-      });
-
-      const auSubtype = generateRandomString("alphaNumeric", 4);
-      const auManufacture = "Myco";
-
-      workerHelper_replaceStrings(newFolderPath, {
-        filePaths: ["Project1/Model/AudioUnitHostModel.swift"],
-        replacements: [
-          { from: `"prj1"`, to: `"${auSubtype}"` },
-          { from: `"Myco"`, to: `"${auManufacture}"` },
-        ],
-      });
-
-      workerHelper_replaceStrings(newFolderPath, {
-        filePaths: ["Project1Extension/info.plist"],
-        replacements: [
-          {
-            from: "<string>prj1</string>",
-            to: `<string>${auSubtype}</string>`,
-          },
-          {
-            from: "<string>Myco</string>",
-            to: `<string>${auManufacture}</string>`,
-          },
-          {
-            from: "<string>Project1Extension</string>",
-            to: `<string>${extensionNameCapital}</string>`,
-          },
-        ],
-      });
-
-      // workerHelper_replaceStrings(newFolderPath, {
-      // 	filePaths: ["project.toml"],
-      // 	replacements: [
-      // 		{ from: "__TEMPLATE_AU_SUBTYPE__", to: auSubtype },
-      // 		{ from: "__TEMPLATE_AU_MANUFACTURER__", to: auManufacture },
-      // 	],
-      // });
-
-      workerHelper_updateFileNamesWithPrefix(newFolderPath, {
-        filePaths: [
-          "Project1/Project1App.swift",
-          "Project1/Project1.entitlements",
-          "Project1",
-          "Project1.xcodeproj",
-          "Project1Extension/Common/Project1Extension-Bridging-Header.h",
-          "Project1Extension",
-        ],
-        originalPrefix: "Project1",
-        newPrefix: projectNameCapital,
-      });
-
+      const templateOptions = await readTemplateOptions();
+      if (templateOptions === "cancelled") {
+        return "cancelled";
+      }
+      const context: TaskContext = {
+        projectName,
+        templateName,
+        options: templateOptions,
+        projectFolderPath: workerHelper_getNewProjectFolderPath(projectName),
+      };
+      instantiateProject(context);
       return true;
     },
   };
