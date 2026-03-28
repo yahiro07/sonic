bfcb06762deb65e57712053bbdd9990023a6cdfd
yahiro <yahiro07@users.noreply.github.com>
2026-03-28 16:19:56 +0900

    separate helper code

--- templates/cpp-multi-target/framework/sonic/platform/vst3/vst_basis/plugin_controller.cpp
+++ templates/cpp-multi-target/framework/sonic/platform/vst3/vst_basis/plugin_controller.cpp
@@ -1,4 +1,5 @@
 #include "./plugin_controller.h"
+#include "../support/controller-parameter-helper.h"
 #include "../support/processor_state_helper.h"
 #include "./plugin-editor-view.h"
 #include "sonic/api/synthesizer-base.h"
@@ -13,37 +14,6 @@ using namespace sonic;
 
 using ParameterItem = sonic::ParameterSpecItem;
 
-static int getParameterVstFlags(const ParameterItem *item) {
-  auto flags = 0;
-  flags |= Steinberg::Vst::ParameterInfo::kCanAutomate;
-  if ((item->flags & ParameterFlags::IsReadOnly) > ParameterFlags::None) {
-    flags |= Steinberg::Vst::ParameterInfo::kIsReadOnly;
-  }
-  if ((item->flags & ParameterFlags::IsHidden) > ParameterFlags::None) {
-    flags |= Steinberg::Vst::ParameterInfo::kIsHidden;
-  }
-  return flags;
-}
-static void addVstParameter(Steinberg::Vst::ParameterContainer &vstParameters,
-                            const ParameterItem &item) {
-  auto step = ParameterSpecHelper::getStepCount(&item);
-  auto normDefaultValue =
-      ParameterSpecHelper::getNormalized(&item, item.defaultValue);
-  auto flags = getParameterVstFlags(&item);
-
-  Steinberg::String paramName;
-  paramName.fromUTF8(
-      reinterpret_cast<const Steinberg::char8 *>(item.label.c_str()));
-
-  vstParameters.addParameter(paramName.text16(), // name
-                             nullptr,            // units
-                             step,             // step count (0 for continuous)
-                             normDefaultValue, // default value (normalized)
-                             flags,            // flags
-                             item.id           // tag (ID)
-  );
-}
-
 tresult PLUGIN_API PluginController::initialize(FUnknown *context) {
   tresult result = EditControllerEx1::initialize(context);
   if (result != kResultOk) {
@@ -55,7 +25,7 @@ tresult PLUGIN_API PluginController::initialize(FUnknown *context) {
     auto parameterItems = parameterBuilder.getItems();
     parameterRegistry.addParameters(parameterItems, 0x7FFFFFFE);
     for (auto &item : parameterItems) {
-      addVstParameter(this->parameters, item);
+      addVstControllerParameter(this->parameters, item);
     }
     controllerParameterPortal.startObserve();
   }
