32aecf569e24c1ad43cc81f79b87f60c472b92b8
yahiro <yahiro07@users.noreply.github.com>
2026-03-29 14:27:17 +0900

    remove log output

--- templates/cpp-multi-target/framework/sonic/platform/vst3/vst_basis/plugin_controller.cpp
+++ templates/cpp-multi-target/framework/sonic/platform/vst3/vst_basis/plugin_controller.cpp
@@ -106,7 +106,6 @@ tresult PLUGIN_API PluginController::getParamValueByString(
 }
 
 tresult PLUGIN_API PluginController::notify(Vst::IMessage *message) {
-  logger.log("PluginController::notify");
   auto consumed = controllerSideMessagePort.applyMessageReceived(message);
   if (consumed) {
     return kResultOk;
