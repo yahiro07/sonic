e096e3a5c3c432c8fe2569a4ef19e3e841b95e06
yahiro <yahiro07@users.noreply.github.com>
2026-04-03 19:08:13 +0900

    update hosts

--- hosts/clap-dev-host/run.sh
+++ hosts/clap-dev-host/run.sh
@@ -1,1 +1,1 @@
-	./build/clap_dev_host/clap_dev_host ../../templates/cpp-multi-target/build/ninja/lib/Debug/project1.clap
+./build/clap_dev_host/clap_dev_host ../../templates/cpp-multi-target/build/ninja-debug/lib/Debug/project1.clap
